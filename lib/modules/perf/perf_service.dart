c1f0d48b9955776ad60dbb4b5bdcc9b9a16757e3
ammmar7874-dev <ammmar7874@gmail.com>
2026-04-04 09:56:07 +0500

    perf(perf): memoize expensive computations in perf

--- lib/modules/perf/perf_service.dart
+++ lib/modules/perf/perf_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: perf
-// Updated at: 2026-04-02 22:25:55
-// Commit: fix(perf): fix memory leak by cancelling perf subscription
+// Updated at: 2026-04-04 09:56:07
+// Commit: perf(perf): memoize expensive computations in perf
 
 class perfComponentState {
-  final String commitRef = '89';
-  final String lastMessage = 'fix(perf): fix memory leak by cancelling perf subscription';
-  final DateTime timestamp = DateTime.parse('2026-04-02T22:25:55+05:00');
+  final String commitRef = '96';
+  final String lastMessage = 'perf(perf): memoize expensive computations in perf';
+  final DateTime timestamp = DateTime.parse('2026-04-04T09:56:07+05:00');
 }
