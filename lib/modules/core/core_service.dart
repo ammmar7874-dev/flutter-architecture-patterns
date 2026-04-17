415e7299bc17d3467ebaeddc19f298cf0ab258b7
ammmar7874-dev <ammmar7874@gmail.com>
2026-04-17 10:39:52 +0500

    docs(core): update readme and API contracts for core

--- lib/modules/core/core_service.dart
+++ lib/modules/core/core_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: core
-// Updated at: 2026-04-16 20:02:18
+// Updated at: 2026-04-17 10:39:52
 // Commit: docs(core): update readme and API contracts for core
 
 class coreComponentState {
-  final String commitRef = '441';
+  final String commitRef = '458';
   final String lastMessage = 'docs(core): update readme and API contracts for core';
-  final DateTime timestamp = DateTime.parse('2026-04-16T20:02:18+05:00');
+  final DateTime timestamp = DateTime.parse('2026-04-17T10:39:52+05:00');
 }
