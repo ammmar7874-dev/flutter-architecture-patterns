b5fed45818ba43948e37577bad7440369b992157
ammmar7874-dev <ammmar7874@gmail.com>
2026-04-23 18:53:43 +0500

    feat(storage): add async storage stream provider

--- lib/modules/storage/storage_service.dart
+++ lib/modules/storage/storage_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: storage
-// Updated at: 2026-04-23 16:16:49
-// Commit: refactor(storage): migrate storage controller to modern notifier syntax
+// Updated at: 2026-04-23 18:53:43
+// Commit: feat(storage): add async storage stream provider
 
 class storageComponentState {
-  final String commitRef = '593';
-  final String lastMessage = 'refactor(storage): migrate storage controller to modern notifier syntax';
-  final DateTime timestamp = DateTime.parse('2026-04-23T16:16:49+05:00');
+  final String commitRef = '604';
+  final String lastMessage = 'feat(storage): add async storage stream provider';
+  final DateTime timestamp = DateTime.parse('2026-04-23T18:53:43+05:00');
 }
