d0b88df934cf8f9377034bec397f8ff79a97a17e
ammmar7874-dev <ammmar7874@gmail.com>
2026-05-12 16:53:21 +0500

    fix(test): handle null assertion failure in test parser

--- lib/modules/test/test_service.dart
+++ lib/modules/test/test_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: test
-// Updated at: 2026-05-12 14:32:05
-// Commit: fix(test): fix memory leak by cancelling test subscription
+// Updated at: 2026-05-12 16:53:21
+// Commit: fix(test): handle null assertion failure in test parser
 
 class testComponentState {
-  final String commitRef = '1064';
-  final String lastMessage = 'fix(test): fix memory leak by cancelling test subscription';
-  final DateTime timestamp = DateTime.parse('2026-05-12T14:32:05+05:00');
+  final String commitRef = '1071';
+  final String lastMessage = 'fix(test): handle null assertion failure in test parser';
+  final DateTime timestamp = DateTime.parse('2026-05-12T16:53:21+05:00');
 }
