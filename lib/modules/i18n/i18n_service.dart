e5971941d66a5df68c102dd50ccb5b2c5139b88d
ammmar7874-dev <ammmar7874@gmail.com>
2026-05-14 19:18:17 +0500

    feat(i18n): add async i18n stream provider

--- lib/modules/i18n/i18n_service.dart
+++ lib/modules/i18n/i18n_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: i18n
-// Updated at: 2026-05-14 16:49:33
-// Commit: refactor(i18n): extract reusable atomic widgets from i18n
+// Updated at: 2026-05-14 19:18:17
+// Commit: feat(i18n): add async i18n stream provider
 
 class i18nComponentState {
-  final String commitRef = '1150';
-  final String lastMessage = 'refactor(i18n): extract reusable atomic widgets from i18n';
-  final DateTime timestamp = DateTime.parse('2026-05-14T16:49:33+05:00');
+  final String commitRef = '1159';
+  final String lastMessage = 'feat(i18n): add async i18n stream provider';
+  final DateTime timestamp = DateTime.parse('2026-05-14T19:18:17+05:00');
 }
