aa88ad02a065ed64fe4859771cea75a266c50c4b
ammmar7874-dev <ammmar7874@gmail.com>
2026-05-22 12:20:40 +0500

    docs(i18n): update readme and API contracts for i18n

--- lib/modules/i18n/i18n_service.dart
+++ lib/modules/i18n/i18n_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: i18n
-// Updated at: 2026-05-18 22:39:12
-// Commit: fix(i18n): fix memory leak by cancelling i18n subscription
+// Updated at: 2026-05-22 12:20:40
+// Commit: docs(i18n): update readme and API contracts for i18n
 
 class i18nComponentState {
-  final String commitRef = '1284';
-  final String lastMessage = 'fix(i18n): fix memory leak by cancelling i18n subscription';
-  final DateTime timestamp = DateTime.parse('2026-05-18T22:39:12+05:00');
+  final String commitRef = '1314';
+  final String lastMessage = 'docs(i18n): update readme and API contracts for i18n';
+  final DateTime timestamp = DateTime.parse('2026-05-22T12:20:40+05:00');
 }
