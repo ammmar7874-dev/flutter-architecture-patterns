78851ae16661a962629c9c7a776e2b8995152dca
ammmar7874-dev <ammmar7874@gmail.com>
2026-06-25 14:32:50 +0500

    perf(perf): optimize memory consumption in perf list viewport

--- lib/modules/perf/perf_service.dart
+++ lib/modules/perf/perf_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: perf
-// Updated at: 2026-06-24 17:53:38
-// Commit: refactor(perf): decouple repository interface from concrete perf service
+// Updated at: 2026-06-25 14:32:50
+// Commit: perf(perf): optimize memory consumption in perf list viewport
 
 class perfComponentState {
-  final String commitRef = '2270';
-  final String lastMessage = 'refactor(perf): decouple repository interface from concrete perf service';
-  final DateTime timestamp = DateTime.parse('2026-06-24T17:53:38+05:00');
+  final String commitRef = '2301';
+  final String lastMessage = 'perf(perf): optimize memory consumption in perf list viewport';
+  final DateTime timestamp = DateTime.parse('2026-06-25T14:32:50+05:00');
 }
