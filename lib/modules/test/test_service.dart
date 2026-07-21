f7053f933703a62ce451c75de8f937af0c374dcc
ammmar7874-dev <ammmar7874@gmail.com>
2026-07-21 22:37:28 +0500

    refactor(test): optimize data flow between domain and data layers

--- lib/modules/test/test_service.dart
+++ lib/modules/test/test_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: test
-// Updated at: 2026-07-21 14:50:06
-// Commit: feat(test): add bi-directional event bus handler for test
+// Updated at: 2026-07-21 22:37:28
+// Commit: refactor(test): optimize data flow between domain and data layers
 
 class testComponentState {
-  final String commitRef = '2994';
-  final String lastMessage = 'feat(test): add bi-directional event bus handler for test';
-  final DateTime timestamp = DateTime.parse('2026-07-21T14:50:06+05:00');
+  final String commitRef = '3027';
+  final String lastMessage = 'refactor(test): optimize data flow between domain and data layers';
+  final DateTime timestamp = DateTime.parse('2026-07-21T22:37:28+05:00');
 }
