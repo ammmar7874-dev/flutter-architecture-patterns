0e2ab8a323f42440caa2b8b0e69931a73bb7e5be
ammmar7874-dev <ammmar7874@gmail.com>
2026-08-11 11:33:55 +0500

    refactor(theme): migrate theme controller to modern notifier syntax

--- lib/modules/theme/theme_service.dart
+++ lib/modules/theme/theme_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: theme
-// Updated at: 2026-08-11 10:26:14
+// Updated at: 2026-08-11 11:33:55
 // Commit: refactor(theme): migrate theme controller to modern notifier syntax
 
 class themeComponentState {
-  final String commitRef = '3517';
+  final String commitRef = '3522';
   final String lastMessage = 'refactor(theme): migrate theme controller to modern notifier syntax';
-  final DateTime timestamp = DateTime.parse('2026-08-11T10:26:14+05:00');
+  final DateTime timestamp = DateTime.parse('2026-08-11T11:33:55+05:00');
 }
