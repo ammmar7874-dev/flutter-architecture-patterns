7bbad58d110efae1a2d09931c7b9475face86bfa
ammmar7874-dev <ammmar7874@gmail.com>
2026-08-14 21:20:11 +0500

    refactor(cache): migrate cache controller to modern notifier syntax

--- lib/modules/cache/cache_service.dart
+++ lib/modules/cache/cache_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: cache
-// Updated at: 2026-08-14 16:48:11
-// Commit: feat(cache): integrate custom telemetry logger in cache
+// Updated at: 2026-08-14 21:20:11
+// Commit: refactor(cache): migrate cache controller to modern notifier syntax
 
 class cacheComponentState {
-  final String commitRef = '3615';
-  final String lastMessage = 'feat(cache): integrate custom telemetry logger in cache';
-  final DateTime timestamp = DateTime.parse('2026-08-14T16:48:11+05:00');
+  final String commitRef = '3620';
+  final String lastMessage = 'refactor(cache): migrate cache controller to modern notifier syntax';
+  final DateTime timestamp = DateTime.parse('2026-08-14T21:20:11+05:00');
 }
