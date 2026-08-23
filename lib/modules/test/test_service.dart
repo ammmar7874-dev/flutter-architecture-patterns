ba2479fd195e12eb3c671ffffc120bb36580046e
ammmar7874-dev <ammmar7874@gmail.com>
2026-08-23 21:42:11 +0500

    fix(test): handle null assertion failure in test parser

--- lib/modules/test/test_service.dart
+++ lib/modules/test/test_service.dart
@@ -1,9 +1,9 @@
 ﻿// Module: test
-// Updated at: 2026-08-23 17:57:32
-// Commit: perf(test): memoize expensive computations in test
+// Updated at: 2026-08-23 21:42:11
+// Commit: fix(test): handle null assertion failure in test parser
 
 class testComponentState {
-  final String commitRef = '3852';
-  final String lastMessage = 'perf(test): memoize expensive computations in test';
-  final DateTime timestamp = DateTime.parse('2026-08-23T17:57:32+05:00');
+  final String commitRef = '3862';
+  final String lastMessage = 'fix(test): handle null assertion failure in test parser';
+  final DateTime timestamp = DateTime.parse('2026-08-23T21:42:11+05:00');
 }
